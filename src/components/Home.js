27fc898e8fe3ff0b482b4f1f850ae4b753d9219b
Shubham Khan <shubhamkhanit@gmail.com>
2022-06-21 23:03:38 +0530

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import OtpInput from './OtpInput';
+
+const Home = () => {
+    const [otp, setOtp] = useState(0);
+    const handleChange = (value) => {
+        setOtp(value);
+        // console.log("OTP>>>>>>"+otp.replace(/ /g, ""));
+        console.log("val>>>>>>"+value);        
+    };
+
+  return (
+    <div>
+        <OtpInput
+          opacity="1"
+          value={otp}
+          onChange={handleChange}
+          numInputs={6}
+          shouldAutoFocus={true}
+          separator={
+            <span>
+              {window.innerWidth < 380 ? (
+                <span>&nbsp;&nbsp;</span>
+              ) : (
+                <span>&nbsp;</span>
+              )}
+            </span>
+          }
+          isInputNum={true}
+          inputStyle={{
+            borderStyle: "ridge",
+            fontSize: "1rem",
+            border: "1px solid #AFB4C5",
+            // borderColor: "#AFB4C5",
+            borderRadius: "8px",
+            WebkitTextFillColor: "#000",
+            width: "2.1rem",
+            height: "2.4rem",
+            fontWeight: "300",
+            backgroundColor: "transparent",
+            outline: "none",
+          }}
+        />
+    </div>
+  )
+}
+
+export default Home
