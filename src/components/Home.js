b8ddb618398481de9ff52ebed73038ac61b6df9d
Shubham Khan <shubhamkhanit@gmail.com>
2022-06-24 15:08:19 +0530

    update sms text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,46 +1,57 @@
 import React, { useState } from 'react';
 import OtpInput from './OtpInput';
 
+const SMS = () => {
+  return(
+    <div>
+      <strong>Send an SMS that includes</strong><br/>
+      <code><small>@otpinput.netlify.app #123456</small></code>
+    </div>
+  )
+}
+
 const Home = () => {
-    const [otp, setOtp] = useState(0);
+    const [otp, setOtp] = useState(0);    
+
     const handleChange = (value) => {
         setOtp(value);
-        // console.log("OTP>>>>>>"+otp.replace(/ /g, ""));
-        console.log("val>>>>>>"+value);        
+        console.log("OTP Value >>>>>> "+value);        
     };
 
   return (
     <div>
-        <OtpInput
-          opacity="1"
-          value={otp}
-          onChange={handleChange}
-          numInputs={6}
-          shouldAutoFocus={true}
-          separator={
-            <span>
-              {window.innerWidth < 380 ? (
-                <span>&nbsp;&nbsp;</span>
-              ) : (
-                <span>&nbsp;</span>
-              )}
-            </span>
-          }
-          isInputNum={true}
-          inputStyle={{
-            borderStyle: "ridge",
-            fontSize: "1rem",
-            border: "1px solid #AFB4C5",
-            // borderColor: "#AFB4C5",
-            borderRadius: "8px",
-            WebkitTextFillColor: "#000",
-            width: "2.1rem",
-            height: "2.4rem",
-            fontWeight: "300",
-            backgroundColor: "transparent",
-            outline: "none",
-          }}
-        />
+      <OtpInput
+        opacity="1"
+        value={otp}
+        onChange={handleChange}
+        numInputs={6}
+        shouldAutoFocus={true}
+        separator={
+          <span>
+            {window.innerWidth < 380 ? (
+              <span>&nbsp;&nbsp;</span>
+            ) : (
+              <span>&nbsp;</span>
+            )}
+          </span>
+        }
+        isInputNum={true}
+        inputStyle={{
+          borderStyle: "ridge",
+          fontSize: "1rem",
+          border: "1px solid #AFB4C5",
+          // borderColor: "#AFB4C5",
+          borderRadius: "8px",
+          WebkitTextFillColor: "#000",
+          width: "2.1rem",
+          height: "2.4rem",
+          fontWeight: "300",
+          backgroundColor: "transparent",
+          outline: "none",
+        }}
+      />
+      <br/>
+      <SMS />
     </div>
   )
 }
